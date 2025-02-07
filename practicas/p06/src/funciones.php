add848e9dfabc1b72dc47fe482dd89189ab3143f
David Tipa <hegbj.jegh15@gmail.com>
2025-02-06 21:57:44 -0600

    ejercicio 6, practica 6 y correccion ejercicio 5

--- practicas/p06/src/funciones.php
+++ practicas/p06/src/funciones.php
@@ -69,6 +69,204 @@ for($i=97;$i<=122;$i++)
     $arreglo[$i]=chr($i);
 }
 ?>
+<?php
 
+$array = array(
+    'UBN6338' => array(
+        'Auto' => array(
+            'marca' => 'HONDA',
+            'modelo' => 2020,
+            'tipo' => 'camioneta'
+        ),
+        'Propietario' => array(
+            'nombre' => 'Alfonzo Esparza',
+            'ciudad' => 'Puebla, Pue.',
+            'direccion' => 'C.U., Jardines de San Manuel'
+        )
+    ),
+    'UBN6339' => array(
+        'Auto' => array(
+            'marca' => 'VW',
+            'modelo' => 2019,
+            'tipo' => 'sedan'
+        ),
+        'Propietario' => array(
+            'nombre' => 'Ma. del Consuelo Molina',
+            'ciudad' => 'Puebla, Pue.',
+            'direccion' => '97 oriente'
+        )
+        ),
+    'JGB7896'=> array(
+        'Auto' => array(
+            'marca' => 'Chevrolet',
+            'modelo' => 2018,
+            'tipo' => 'sedan'
+        ),
+        'Propietario' => array(
+            'nombre' => 'Juan Carlos',
+            'ciudad' => 'Puebla, Pue.',
+            'direccion' => 'San Manuel'
+        )
+        ),
+    'JGB7823'=> array(
+        'Auto' => array(
+            'marca' => 'BMW',
+            'modelo' => 2010,
+            'tipo' => 'sedan'
+        ),
+        'Propietario' => array(
+            'nombre' => 'Miguel angel',
+            'ciudad' => 'Ciudad de México',
+            'direccion' => 'calle la ballenita'
+        )
+        ), 
+    'JGB7824'=> array(
+        'Auto' => array(
+            'marca' => 'Audi',
+            'modelo' => 2015,
+            'tipo' => 'sedan'
+        ),
+        'Propietario' => array(
+            'nombre' => 'Luisa',
+            'ciudad' => 'Tonala, Chis.',
+            'direccion' => 'San Miguel de Totanaca'
+        )
+        ), 
+    'JGB7825'=> array(
+        'Auto' => array(
+            'marca' => 'Nissan',
+            'modelo' => 2017,
+            'tipo' => 'sedan'
+        ),
+        'Propietario' => array(
+            'nombre' => 'Piratita',
+            'ciudad' => 'Tuxtla Gutierrez, Chis.',
+            'direccion' => 'De las totoncas apasiwuere'
+        )
+        ),
+    'JGB7826'=> array(
+        'Auto' => array(
+            'marca' => 'Ford',
+            'modelo' => 2016,
+            'tipo' => 'sedan'
+        ),
+        'Propietario' => array(
+            'nombre' => 'Kima',
+            'ciudad' => 'Monterrey, Nuevo Leon.',
+            'direccion' => 'la chiwuis ciudad'
+        )
+        ),
+    'JGB7827'=> array(
+        'Auto' => array(
+            'marca' => 'Toyota',
+            'modelo' => 2014,
+            'tipo' => 'carga'
+        ),
+        'Propietario' => array(
+            'nombre' => 'Ikonic Ramirez',
+            'ciudad' => 'Tlaxacala, Tlax.',
+            'direccion' => 'la chona'
+        )
+        ),
+    'JGB7828'=> array(
+        'Auto' => array(
+            'marca' => 'Mazda',
+            'modelo' => 2013,
+            'tipo' => 'Trocona'
+        ),
+        'Propietario' => array(
+            'nombre' => 'Tomas de Aquino',
+            'ciudad' => 'Villaflores, Chis.',
+            'direccion' => 'Calle pentelico 180-4'
+        )
+    ),
+    'JGB7829'=> array(
+        'Auto' => array(
+            'marca' => 'Mercedes Benz',
+            'modelo' => 2012,
+            'tipo' => 'mamalona'
+        ),
+        'Propietario' => array(
+            'nombre' => 'skibidi',
+            'ciudad' => 'Buenavista. Madrid',
+            'direccion' => 'San Miguel de Totanaca'
+        )
+        ),
+    'JGB7830'=> array(
+        'Auto' => array(
+            'marca' => 'KIA',
+            'modelo' => 2011,
+            'tipo' => 'sedan'
+        ),
+        'Propietario' => array(
+            'nombre' => 'cafecito de las totoncas',
+            'ciudad' => 'Apasowuere, Chis.',
+            'direccion' => 'Texcoco, calle tilin s/n'
+        )
+        ),'JGB7831' => array(
+        'Auto' => array(
+            'marca' => 'Hyundai',
+            'modelo' => 2021,
+            'tipo' => 'SUV'
+        ),
+        'Propietario' => array(
+            'nombre' => 'Carlos Perez',
+            'ciudad' => 'Guadalajara, Jal.',
+            'direccion' => 'Av. Chapultepec 123'
+        )
+    ),
+    'JGB7832' => array(
+        'Auto' => array(
+            'marca' => 'Tesla',
+            'modelo' => 2022,
+            'tipo' => 'eléctrico'
+        ),
+        'Propietario' => array(
+            'nombre' => 'Ana Gomez',
+            'ciudad' => 'Monterrey, NL',
+            'direccion' => 'Calle Reforma 456'
+        )
+    ),
+    'JGB7833' => array(
+        'Auto' => array(
+            'marca' => 'Ferrari',
+            'modelo' => 2020,
+            'tipo' => 'deportivo'
+        ),
+        'Propietario' => array(
+            'nombre' => 'Luis Martinez',
+            'ciudad' => 'Cancún, QR',
+            'direccion' => 'Blvd. Kukulcan 789'
+        )
+    ),
+    'JGB7834' => array(
+        'Auto' => array(
+            'marca' => 'Lamborghini',
+            'modelo' => 2021,
+            'tipo' => 'deportivo'
+        ),
+        'Propietario' => array(
+            'nombre' => 'Maria Lopez',
+            'ciudad' => 'Tijuana, BC',
+            'direccion' => 'Calle Revolución 101'
+        )
+    )
+);
+function buscarPorMatricula($matricula) {
+    global $array;
+    if (isset($array[$matricula])) {
+        return $array[$matricula];
+    } else {
+        return null;
+    }
+}
+
+
+function obtenerTodosLosAutos() {
+    global $array;
+    return $array;
+}
+?>
+?>
 
 
