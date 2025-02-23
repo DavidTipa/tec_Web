c60dc63e10e97435574888c143500996e185690a
David Tipa <hegbj.jegh15@gmail.com>
2025-02-22 22:36:17 -0600

    practica 8 completa

--- practicas/p08/xhtml.php
+++ practicas/p08/xhtml.php
@@ -3,34 +3,28 @@
 <html xmlns="http://www.w3.org/1999/xhtml" xml:lang="es">
 <head>
     <meta http-equiv="Content-Type" content="text/html; charset=UTF-8" />
-    <title>Productos</title>
+    <title>Productos Vigentes</title>
     <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.3.1/css/bootstrap.min.css" integrity="sha384-ggOyR0iXCbMQv3Xipma34MD+dH/1fQ784/j6cY/iJTQUOhcWr7x9JvoRxT2MZw1T" crossorigin="anonymous">
 </head>
 <body>
     <div class="container mt-4">
-        <h3 class="text-center">Lista de Productos</h3>
+        <h3 class="text-center">Lista de Productos Vigentes</h3>
         <br/>
 
         <?php
-       
-        if(isset($_GET['tope'])) {
-            $tope = (int) $_GET['tope'];
-        } else {
-            die('<div class="alert alert-danger">Parámetro "tope" no detectado...</div>');
-        }
-
-        
+        // Conexión a la base de datos
         @$link = new mysqli('localhost', 'root', 'OlakeaZe1', 'marketzone');
         
-        
-        if ($link->connect_errno) 
-        {
+        // Verificar conexión
+        if ($link->connect_errno) {
             die('<div class="alert alert-danger">Falló la conexión: '.$link->connect_error.'</div>');
         }
 
-             $query = "SELECT * FROM productos WHERE unidades <= $tope";
+        // Consulta para obtener productos no eliminados (eliminado != 0)
+        $query = "SELECT * FROM productos WHERE eliminado = 0";
         $result = $link->query($query);
         
+        // Verificar si hay resultados
         if ($result->num_rows > 0): ?>
             <table class="table table-bordered">
                 <thead class="thead-dark">
@@ -63,10 +57,10 @@
                 </tbody>
             </table>
         <?php else: ?>
-            <div class="alert alert-warning">No hay productos disponibles con unidades menores o iguales a <?= $tope ?>.</div>
+            <div class="alert alert-warning">No hay productos vigentes disponibles.</div>
         <?php endif;
         
-        
+        // Liberar resultados y cerrar conexión
         $result->free();
         $link->close();
         ?>
